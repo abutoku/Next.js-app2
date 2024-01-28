615f055ba7c77ed4feadbd78be0bb9562eb76ad3
atsushi.yasutake <atsu-yasutake@wearcoord.com>
2024-01-28 09:07:00 +0900

    first commit

---
+++ src/app/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='py-2 px-4 border-t'>
+      <small>©2023 neko blog</small>
+    </footer>
+  )
+}
+
+export default Footer;
